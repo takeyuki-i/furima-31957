c113a0186598bd4d1a21b0dd0dc0507173f48335
takeyuki-i <golden_eggs7777@yahoo.co.jp>
2021-02-01 06:27:39 +0900

    itemモデル作成、マイグレーション記述

---
+++ db/migrate/20210131212337_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string  :name             ,null: false
+      t.text    :text             ,null: false
+      t.integer :category_id      ,null: false
+      t.integer :state_id         ,null: false
+      t.integer :delivery_fee_id  ,null: false
+      t.integer :delivery_area_id ,null: false
+      t.integer :delivery_time_id ,null: false
+      t.integer :price            ,null: false
+      t.timestamps
+    end
+  end
+end
