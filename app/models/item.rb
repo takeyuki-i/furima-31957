0e631b974ef7ad174fc7a0115c5a861602238e4f
takeyuki-i <golden_eggs7777@yahoo.co.jp>
2021-02-01 06:39:16 +0900

    [Activehash]categoryモデル作成・アソシエーション・バリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+
+extend ActiveHash::Associations::ActiveRecordExtensions
+belongs_to :category
+belongs_to :state
+belongs_to :deliveryfee
+belongs_to :deliveryarea
+belongs_to :deliverytime
+
+validates :item_name       , :string , presence: true
+validates :text            , :text   , presence: true
+validates :category_id     , numericality: { other_than: 1 } 
+validates :state_id        , numericality: { other_than: 1 } 
+validates :deliveryfee_id  , numericality: { other_than: 1 } 
+validates :deliveryarea_id , numericality: { other_than: 1 } 
+validates :deliverytime_id , numericality: { other_than: 1 } 
+validates :price           , :integer , presence: true
+
 end
