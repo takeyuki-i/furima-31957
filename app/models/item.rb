dba6ae805bb2c3ebf81428912d651ba7a1a5e0e9
takeyuki-i <golden_eggs7777@yahoo.co.jp>
2021-02-01 06:47:27 +0900

    [Activehash]delivery_fee モデル作成・アソシエーション・バリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,17 +3,17 @@ class Item < ApplicationRecord
 extend ActiveHash::Associations::ActiveRecordExtensions
 belongs_to :category
 belongs_to :state
-belongs_to :deliveryfee
-belongs_to :deliveryarea
-belongs_to :deliverytime
+belongs_to :delivery_fee
+belongs_to :delivery_area
+belongs_to :delivery_time
 
-validates :item_name       , :string , presence: true
-validates :text            , :text   , presence: true
-validates :category_id     , numericality: { other_than: 1 } 
-validates :state_id        , numericality: { other_than: 1 } 
-validates :deliveryfee_id  , numericality: { other_than: 1 } 
-validates :deliveryarea_id , numericality: { other_than: 1 } 
-validates :deliverytime_id , numericality: { other_than: 1 } 
-validates :price           , :integer , presence: true
+validates :item_name        , :string , presence: true
+validates :text             , :text   , presence: true
+validates :category_id      , numericality: { other_than: 1 }
+validates :state_id         , numericality: { other_than: 1 }
+validates :delivery_fee_id  , numericality: { other_than: 1 }
+validates :delivery_area_id , numericality: { other_than: 1 }
+validates :delivery_time_id , numericality: { other_than: 1 }
+validates :price            , :integer , presence: true
 
 end
