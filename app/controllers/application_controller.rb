af88bdf4a3fd405c4c1cfbee4c4355979b5597c6
takeyuki-i <golden_eggs7777@yahoo.co.jp>
2021-02-05 06:54:34 +0900

    データベース作成・ストロングパラメーター設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+before_action :devise_parameters, if: :devise_controller?
+
+private
+def devise_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,:last_name,:first_name_kana,:last_name_kana,:birthday])
+end
+
 end
