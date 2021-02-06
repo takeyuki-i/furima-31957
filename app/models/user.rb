23092bcf84788c917aad8a6973d14312c71c8c1a
takeyuki-i <golden_eggs7777@yahoo.co.jp>
2021-02-06 17:18:19 +0900

    ユーザー管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  validates :nickname , presence: true
+  validates :email , presence: true
+  validates :password , presence: true , format: { with: /\A[a-zA-Z0-9]+\z{6,}/ }
+  validates :encrypted_password , presence: true , format: { with: /\A[a-zA-Z0-9]+\z{6,}/ }
+  validates :first_name , presence: true , format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+  validates :last_name , presence: true , format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+  validates :first_name_kana , presence: true , format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kana , presence: true , format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday , presence: true
 
 
 end
