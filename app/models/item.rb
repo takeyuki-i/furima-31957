2a69e1f7a9cc5c8b38d3ff2c0f751226eccd78e5
takeyuki-i <golden_eggs7777@yahoo.co.jp>
2021-02-11 11:51:50 +0900

    itemのアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+extend ActiveHash::Associations::ActiveRecordExtensions
+belongs_to :category
+belongs_to :state
+belongs_to :delivery_fee
+belongs_to :delivery_area
+belongs_to :delivery_time
+has_one_attached :image
+
+validates :name             , :string , presence: true
+validates :text             , :text   , presence: true
+validates :category_id      , numericality: { other_than: 1 }
+validates :state_id         , numericality: { other_than: 1 }
+validates :delivery_fee_id  , numericality: { other_than: 1 }
+validates :delivery_area_id , numericality: { other_than: 1 }
+validates :delivery_time_id , numericality: { other_than: 1 }
+validates :price            , :integer , presence: true
+
 end
