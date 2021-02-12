4789167406d83e0473b8a2bc06b43da1b2838d62
takeyuki-i <golden_eggs7777@yahoo.co.jp>
2021-02-12 15:19:52 +0900

    Faker設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name             {Faker::Lorem.sentence}
+    text             {Faker::Lorem.sentence}
+    category_id      {'2'}
+    state_id         {'2'}
+    delivery_fee_id  {'2'}
+    delivery_area_id {'2'}
+    delivery_time_id {'2'}
+    price            {'1000000'}
+  end
+
+  after(:build) do |message|
+    message.image.attach(io: File.open('public/images/test.png'), filename: 'test.png')
   end
 end
+
+
