11667e3e2726f88378e3de52300d97334fa4e0a1
takeyuki-i <golden_eggs7777@yahoo.co.jp>
2021-02-15 21:03:33 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_action :devise_parameters, if: :devise_controller?
 
   private
