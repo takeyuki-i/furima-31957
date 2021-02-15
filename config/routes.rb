28d0016f36d4f9ad46234e3e12d92ad08e777009
takeyuki-i <golden_eggs7777@yahoo.co.jp>
2021-02-15 22:04:08 +0900

    商品詳細の表示記述完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'items#index'
-  resources :items , only: [:index,:new,:create]
+  resources :items , only: [:index,:new,:create,:show]
   devise_for :users
 end
