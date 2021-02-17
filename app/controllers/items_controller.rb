54a917c1d606d1ca742eb377338bde8db61bb990
takeyuki-i <golden_eggs7777@yahoo.co.jp>
2021-02-17 23:53:07 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,7 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     unless user_signed_in? && current_user.id == @item.user_id
       redirect_to root_path
     end
