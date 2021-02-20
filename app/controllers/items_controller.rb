5474143394cf9354414d845fa63f8af1158c8dd6
takeyuki-i <golden_eggs7777@yahoo.co.jp>
2021-02-20 11:28:49 +0900

    削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy, :update]
-  before_action :set_item,only: [:show,:edit,:update]
+  before_action :set_item,only: [:show,:edit,:update,:destroy]
   before_action :move_to_index,only:[:edit,:update]
 
   def index
@@ -31,6 +31,12 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
     end
   end
 
